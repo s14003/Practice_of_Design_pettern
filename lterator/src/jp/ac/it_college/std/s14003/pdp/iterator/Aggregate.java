af1f0f9d35f19b3f7b6b0507cefaf19a26790bb0
s14003 <s14003@std.it-college.ac.jp>
2015-06-02 12:00:04 +0900

    名前修正

--- lterator/src/jp/ac/it_college/std/s14003/pdp/iterator/Aggregate.java
+++ lterator/src/jp/ac/it_college/std/s14003/pdp/iterator/Aggregate.java
@@ -4,5 +4,6 @@ package jp.ac.it_college.std.s14003.pdp.iterator;
  * Created by s14003 on 15/06/02.
  */
 public interface Aggregate {
-    public abstract iterator iterator();
+    public abstract Iterator iterator();
+
 }
--- lterator/src/jp/ac/it_college/std/s14003/pdp/iterator/Iterator.java
+++ lterator/src/jp/ac/it_college/std/s14003/pdp/iterator/Iterator.java
@@ -5,7 +5,7 @@ package jp.ac.it_college.std.s14003.pdp.iterator;
 /**
  * Created by s14003 on 15/06/02.
  */
-public interface iterator {
+public interface Iterator {
     public abstract boolean hasNext();
-    public abstract Object next();
+    public abstract Object next();//何でも返せるようにしている
 }

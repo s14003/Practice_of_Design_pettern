0d2caedb92da9cf95c342ddae5530f21f507e130
s14003 <s14003@std.it-college.ac.jp>
2015-06-11 15:20:12 +0900

    13章終わり

--- src/jp/ac/it_college/std/s14003/pdp/Main.java
+++ src/jp/ac/it_college/std/s14003/pdp/Main.java
@@ -1,12 +1,19 @@
 package jp.ac.it_college.std.s14003.pdp;
 
-import jp.ac.it_college.std.s14003.pdp.adapter.Print;
 import jp.ac.it_college.std.s14003.pdp.adapter.Print2;
-import jp.ac.it_college.std.s14003.pdp.adapter.PrintBanner;
 import jp.ac.it_college.std.s14003.pdp.adapter.PrintBanner2;
+import jp.ac.it_college.std.s14003.pdp.af.factory.Link;
+import jp.ac.it_college.std.s14003.pdp.af.factory.Page;
+import jp.ac.it_college.std.s14003.pdp.af.factory.Tray;
+import jp.ac.it_college.std.s14003.pdp.bridge.CountDisplay;
+import jp.ac.it_college.std.s14003.pdp.bridge.Display;
+import jp.ac.it_college.std.s14003.pdp.bridge.StringDisplayImpl;
 import jp.ac.it_college.std.s14003.pdp.builder.Director;
 import jp.ac.it_college.std.s14003.pdp.builder.HTMLBuilder;
 import jp.ac.it_college.std.s14003.pdp.builder.TextBuilder;
+import jp.ac.it_college.std.s14003.pdp.composite.Directory;
+import jp.ac.it_college.std.s14003.pdp.composite.File;
+import jp.ac.it_college.std.s14003.pdp.composite.FileTreatmentException;
 import jp.ac.it_college.std.s14003.pdp.factory.framework.Factory;
 import jp.ac.it_college.std.s14003.pdp.factory.framework.Product;
 import jp.ac.it_college.std.s14003.pdp.factory.idcard.IDCardFactory;
@@ -18,6 +25,10 @@ import jp.ac.it_college.std.s14003.pdp.prototype.MessageBox;
 import jp.ac.it_college.std.s14003.pdp.prototype.UnderlinePen;
 import jp.ac.it_college.std.s14003.pdp.prototype.framework.Manager;
 import jp.ac.it_college.std.s14003.pdp.singleton.Singleton;
+import jp.ac.it_college.std.s14003.pdp.strategy.Hand;
+import jp.ac.it_college.std.s14003.pdp.strategy.Player;
+import jp.ac.it_college.std.s14003.pdp.strategy.ProbStrategy;
+import jp.ac.it_college.std.s14003.pdp.strategy.WinningStrategy;
 import jp.ac.it_college.std.s14003.pdp.template.AbstractDisplay;
 import jp.ac.it_college.std.s14003.pdp.template.CharDisplay;
 import jp.ac.it_college.std.s14003.pdp.template.StringDisplay;
@@ -39,9 +50,13 @@ public class Main {
         //templateTest();
         //factorMain();
         //factoryMain();
-        builderMain(new String[] {"html"});
-
-
+        //builderMain(new String[] {"html"});
+        //adstractMain(new String[] {"jp.ac.it_college.std.s14003.pdp.af.listfactory.ListFactory"});
+        //bridgeMain();
+        //strategyMain(new String[] {"314", "15"});
+        //compositeMain();
+        //jp.ac.it_college.std.s14003.pdp.decorator.Main.main(args);
+        jp.ac.it_college.std.s14003.pdp.visitor.Main.main(args);
     }
 
     public static void iteratorMain() {
@@ -70,12 +85,6 @@ public class Main {
             Book book = (Book)it.next();
             System.out.println(book.getName());
         }
-    }
-
-    public  static void adapterMain() {
-        Print p = new PrintBanner("Main");
-        p.printWeak();
-        p.printStrong();
     }
 
     public static void adapterMain2() {
@@ -372,10 +381,126 @@ public class Main {
         }
 
     }
+
     public static void Builderusage() {
         System.out.println("Usage: java Main plain 　　　プレーンテキストで作成");
         System.out.println("Usage: java Main html　　　　HTMLファイルで文書作成");
     }
 
+    public static void adstractMain(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Main class.name.of.ConcreteFactory");
+            System.out.println("Example 1: java Main listfactory.ListFactory");
+            System.out.println("Example 2: java Main tablefactory.TableFactory");
+            System.exit(0);
+        }
 
+        jp.ac.it_college.std.s14003.pdp.af.factory.Factory factory
+                = jp.ac.it_college.std.s14003.pdp.af.factory.Factory.getFactory(args[0]);
+
+        Link asahi = factory.createLink("朝日新聞", "http://www.asahi.com/");
+        Link yomiuri = factory.createLink("読売新聞", "http://www.yomiuri.co.jp/");
+
+        Link us_yahoo = factory.createLink("Yahoo!", "http://www.yahoo.com/");
+        Link jp_yahoo = factory.createLink("Yahoo!Japan","http://www.yahoo.co.jp/");
+        Link excite = factory.createLink("Excite","http://www.excite.com/");
+        Link google = factory.createLink("Google","http://www.google.com/");
+
+        Tray traynews = factory.createTray("新聞");
+        traynews.add(asahi);
+        traynews.add(yomiuri);
+
+        Tray trayyahoo = factory.createTray("Yahoo!");
+        trayyahoo.add(us_yahoo);
+        trayyahoo.add(jp_yahoo);
+
+        Tray traysearch = factory.createTray("サーチエンジン");
+        traysearch.add(trayyahoo);
+        traysearch.add(excite);
+        traysearch.add(google);
+
+        Page page = factory.createPage("LinkPage","結城浩");
+        page.add(traynews);
+        page.add(traysearch);
+        page.output();
+    }
+
+    public static void bridgeMain() {
+        Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
+        Display d2 = new Display(new StringDisplayImpl("Hello, Spain."));
+        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+        d1.display();
+        d2.display();
+        d3.display();
+        d3.multiDisplay(5);
+    }
+
+    public static void strategyMain(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java Main randomseed1 randomseed2");
+            System.out.println("Example: java Main 314 15");
+            System.exit(0);
+        }
+        int seed1 = Integer.parseInt(args[0]);
+        int seed2 = Integer.parseInt(args[1]);
+        Player player1 = new Player("Taro", new WinningStrategy(seed1));
+        Player player2 = new Player("Hana", new ProbStrategy(seed2));
+        for (int i = 0;i < 1000; i++ ) {
+            Hand nextHand1 = player1.nextHand();
+            Hand nextHand2 = player2.nextHand();
+            if (nextHand1.isStrongerThan(nextHand2)) {
+                System.out.println("Winner:" + player1);
+                player1.win();
+                player2.lose();
+            } else if (nextHand2.isStrongerThan(nextHand1)) {
+                System.out.println("Winner:" + player2);
+                player2.win();
+                player1.lose();
+            } else {
+                System.out.println("Even......");
+                player1.even();
+                player2.even();
+            }
+        }
+        System.out.println("Total result:");
+        System.out.println(player1.toString());
+        System.out.println(player2.toString());
+    }
+
+    public static void compositeMain() {
+        try {
+            System.out.println("Making root entries....");
+            Directory rootdir = new Directory("root");
+            Directory bindir = new Directory("bin");
+            Directory tmpdir = new Directory("tmp");
+            Directory usrdir = new Directory("usr");
+            rootdir.add(bindir)
+                    .add(tmpdir)
+                    .add(usrdir);
+            bindir.add(new File("vi", 10000))
+                    .add(new File("latex", 20000));
+            rootdir.printList();
+
+            System.out.println();
+            System.out.println("Making user entries....");
+            Directory yuki = new Directory("yuki");
+            Directory hanako = new Directory("hanako");
+            Directory tomura = new Directory("tomura");
+            usrdir.add(yuki)
+                    .add(hanako)
+                    .add(tomura);
+            yuki.add(new File("dialy.html", 100))
+                    .add(new File("Composite.java", 200));
+            hanako.add(new File("memo.tex", 300));
+            tomura.add(new File("game.doc", 300))
+                    .add(new File("junk.mail", 500));
+            rootdir.printList();
+
+
+        } catch (FileTreatmentException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
 }
